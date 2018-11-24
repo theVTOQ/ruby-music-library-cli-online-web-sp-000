aecdea73996e55cc1e64b24e8ad94292672dced7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-24 22:39:13 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -9,11 +9,16 @@ module Memorable
       new_instance.save
       new_instance
     end
+
+    def sort
+      self.all.sort_by{|element| element.name}
+    end
   end
 
   module InstanceMethods
     def save
       self.class.all << self
+      self.class.all.sort
     end
   end
 end

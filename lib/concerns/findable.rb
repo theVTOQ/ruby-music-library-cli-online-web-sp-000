39af64bb34f6d5f7e451f51050716ee0034c3ed3
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-24 20:31:51 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Findable
   def find_by_name(name)
-    self.all.detect {element.name = name}
+    self.all.detect {element.name.downcase = name.downcase}
   end
 
   def find_or_create_by_name(name)

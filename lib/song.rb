c37021e828e5709418a44107bab18c08e9f42fdd
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-24 21:36:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist = artist
-    #self.genre = genre
+    self.genre = genre
     #@@all << self
   end
 

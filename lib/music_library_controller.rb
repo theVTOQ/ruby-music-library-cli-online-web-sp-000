f4ed342b12344530a30a33717d948b6614ae1d56
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-24 23:04:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -98,7 +98,7 @@ class MusicLibraryController
     #binding.pry
     if song_number < Song.all.size && song_number > 0
 
-      song = Song.all.sort_by{|song| song.name}[song_number]
+      song = Song.all.sort{|a, b| b.name <=> a.name}[song_number]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end

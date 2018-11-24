fb5148dbb82d6e2dc119768d0dd0a940e3dac739
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-24 21:00:57 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,4 +73,18 @@ class MusicLibraryController
       end
     end
   end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    artist_name = gets
+    artist = Artist.find_by_name(artist_name)
+    if artist == nil
+      list_songs_by_artist
+    else
+      artist.songs.sort_by!{|song| song.name}.each do |song|
+        puts "#{count}. #{artist.name} - #{song.name} - #{genre.name}"
+        count += 1
+      end
+    end
+  end
 end
